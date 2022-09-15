a9ec44f0700c1a5d3c0215c1017d9b0419e5aba7
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-09-15 20:49:28 +0700

    feat(prelude): added context ctrl-c

---
+++ prelude/context.go
@@ -0,0 +1,41 @@
+package prelude
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+)
+
+var (
+	once         = &sync.Once{}
+	ctrlcContext = context.Background()
+)
+
+/*
+First call to this function will make a signal capturer that will catch SIGINT (CTRL+C)
+and a new context that will receive Done signal when SIGINT signal is received,
+then that context will be returned
+
+Subsequent calls will not make a new signal capturer and
+only returns the same context as the first created context.
+*/
+func GetCtrlCContext() context.Context {
+	once.Do(func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+
+		ctx, exit := context.WithCancel(ctrlcContext)
+		ctrlcContext = ctx
+
+		go func() {
+			for sig := range c {
+				fmt.Printf("\n[Prelude] Received Signal: %s\n", sig)
+				exit()
+			}
+		}()
+	})
+
+	return ctrlcContext
+}
